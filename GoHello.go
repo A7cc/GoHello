610a361616cf2c63c209008d9f2d5837d2c73f9c
a7cc <1123511349@qq.com>
2022-09-19 17:02:07 +0800

    添加了交互

--- GoHello.go
+++ GoHello.go
@@ -2,6 +2,7 @@ package GoHello
 
 import "fmt"
 
-func PrintHello() {
+func PrintHello(name string) {
 	fmt.Println("Hello World！这是Go的测试库！")
+	fmt.Println("你好！用户：", name)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/A7cc/GoHello
+module github.com/A7cc/GoHello/v2
 
 go 1.18
